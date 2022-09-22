a4e43d777d33efd08de8c49586251d2814293e09
Bruno Andrade <bruno.balint@gmail.com>
2022-09-22 01:24:04 -0300

    add gorilla/mux router

--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/bandrade/golang-simple-restapi/controllers"
+	"github.com/gorilla/mux"
 )
 
 func HandleResquest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalities", controllers.AllPersonalities)
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalities", controllers.AllPersonalities)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
