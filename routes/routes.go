43c3b3fda27cfdf303cacabf9001f39a96e24ddd
Bruno Andrade <bruno.balint@gmail.com>
2022-09-23 11:40:26 -0300

    Adding content-type header

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/bandrade/golang-simple-restapi/controllers"
+	"github.com/bandrade/golang-simple-restapi/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities/", controllers.AllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.FindPersonalityById).Methods("Get")
